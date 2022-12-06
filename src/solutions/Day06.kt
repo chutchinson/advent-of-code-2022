e6414bc2fb14ae9dabb3eb12913b7f901bfeae80
Chris Hutchinson <chutchinson@dynics.com>
2022-12-06 00:34:45 -0500

    day 6

---
+++ src/solutions/Day06.kt
@@ -0,0 +1,22 @@
+package com.cshutchinson.adventofcode2022
+
+class Day06 : Solver {
+    override fun solve (input: Sequence<String>) {
+        val datastream = input.joinToString("")
+        println(first(datastream))
+        println(second(datastream))
+    }
+    
+    fun findMarker (input: String, size: Int): Int {
+        for (index in 0..input.length) {
+            val slice = input.substring(index, index + size)
+            if (slice.toSet().size == size) {
+                return index + size
+            }
+        }
+        return -1
+    }
+
+    fun first (input: String) = findMarker(input, 4)
+    fun second (input: String) = findMarker(input, 14)
+}
